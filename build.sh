99db55d6c866b853e58a7c0f883a8ea1b94942c9
liuziangexit <liuziang@liuziangexit.com>
2022-12-29 15:03:40 +0800

    build scripts

---
+++ build.sh
@@ -0,0 +1,8 @@
+set -e
+
+cmake -DCMAKE_BUILD_TYPE="RELEASE" -S llvm -B build -G 'Unix Makefiles' \
+-DLLVM_ENABLE_PROJECTS="clang;compiler-rt" -DCMAKE_INSTALL_PREFIX=$(pwd)/install \
+-DLLVM_ENABLE_ASSERTIONS=On
+
+cd build
+make -j$(getconf _NPROCESSORS_ONLN) VERBOSE=1
